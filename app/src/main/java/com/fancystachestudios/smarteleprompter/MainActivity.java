8cde253b894e51bbf3223af8ac928417d4109f41
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2018-11-06 15:11:10 -0800

    Make main page UI

--- app/src/main/java/com/fancystachestudios/smarteleprompter/MainActivity.java
+++ app/src/main/java/com/fancystachestudios/smarteleprompter/MainActivity.java
@@ -1,16 +1,61 @@
 package com.fancystachestudios.smarteleprompter;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.crashlytics.android.Crashlytics;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    @BindView(R.id.main_fab)
+    FloatingActionButton FAB;
+    @BindView(R.id.main_spinner_sort)
+    Spinner sortSpinner;
+
+    String selectedSort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("naputest", "FAB clicked");
+            }
+        });
+
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.main_spinner_items,
+                android.R.layout.simple_spinner_dropdown_item);
+        sortSpinner.setAdapter(spinnerAdapter);
+        sortSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        selectedSort = parent.getItemAtPosition(pos).toString();
+        Log.d("naputest", selectedSort);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
